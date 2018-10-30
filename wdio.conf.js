8d38db2b9ab3628adc21b399d1a63d3163967159
Daniel Schonfeld <danyschonfeld@hotmail.com>
2018-10-30 03:47:31 -0700

    moved loading of chai assert library to before hook

--- wdio.conf.js
+++ wdio.conf.js
@@ -160,8 +160,9 @@ exports.config = {
      * @param {Array.<Object>} capabilities list of capabilities details
      * @param {Array.<String>} specs List of spec file paths that are to be run
      */
-    // before: function (capabilities, specs) {
-    // },
+    before: function (capabilities, specs) {
+        assert = require('chai').assert;
+    },
     /**
      * Runs before a WebdriverIO command gets executed.
      * @param {String} commandName hook command name
