f62ca14ab07c26fdf5c39a7ef47a6e63b8fde6cb
Daniel Schonfeld <danyschonfeld@hotmail.com>
2018-10-30 03:24:30 -0700

    added chai assertions

--- test/sync_navigation_test.js
+++ test/sync_navigation_test.js
@@ -1,20 +1,24 @@
+var assert = require('chai').assert;
+
 describe('Navigation', function() {
     describe('Shop Button', function() {
-        it('should redirect to /products page when clicked', function() {
+        it('should redirect to /product-page when clicked', function() {
             browser.url('./');
             console.log("1. Navigated to URL:" + browser.getUrl());
 
             var title = browser.getTitle();
-            console.log('Page title: ' + title);
+            console.log('on page titled: ' + title);
 
             browser.click('.shop-callout a');
             console.log('2. Clicked Shop button');
 
             var title2 = browser.getTitle();
-            console.log('Page Title:' + title2);
+            console.log('on page titled:' + title2);
             
             var url = browser.getUrl();
-            console.log('Url: ' + url);
+            console.log('shop button redirected user to: ' + url);
+
+            assert.include(url, '/product-page.html', 'Shop button did not redirect user to expected url');
         })
     })
 })
