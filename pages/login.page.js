1238c820980f1752fb4ee7e0ac05019541be8c4b
Daniel Schonfeld <danyschonfeld@hotmail.com>
2018-10-31 02:47:37 -0700

    Update login.page.js

--- pages/login.page.js
+++ pages/login.page.js
@@ -1,9 +1,15 @@
 class LoginPage {
   
-  get usernameField() { return ${"#username"}; }
-  get passwordField() { return ${"#password"}; }
-  get loginButton() { return ${".fa-sign-in"}; }
+  get usernameField() { return browser.element{"#username"}; }   // $ = browser.element   and    $$ = browser.elements (returns an array)
+  get passwordField() { return browser.element{"#password"}; }
+  get loginButton() { return browser.element{".fa-sign-in"}; }
   
+  
+  submitLoginForm(username, password) {
+    browser.setValue(this.usernameField, username);
+    browser.setValue(this.passwordField, password);
+    browser.click(this.loginButton);
+  }  
   
 }
 
