59ec76559c1b396932cbf8e3a217ae9f27ad660a
Daniel Schonfeld <danyschonfeld@hotmail.com>
2018-10-31 01:02:08 -0700

    found a better testing site for practice

--- wdio.conf.js
+++ wdio.conf.js
@@ -77,7 +77,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-    baseUrl: 'http://127.0.0.1:8303',
+    baseUrl: 'https://the-internet.herokuapp.com/login',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
